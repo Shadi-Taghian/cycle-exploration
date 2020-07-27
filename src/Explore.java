47aefd7a3834e1259a73f509dab54fcdadd7deaf
shaditaghian <shadi.taghian@gmail.com>
2020-07-27 14:00:03 -0400

    dp completed

--- src/Explore.java
+++ src/Explore.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -5,39 +8,45 @@ import java.util.Scanner;
 
 public class Explore {
     public static ArrayList<int[][]> cycles = new ArrayList<>();
-    public static  int[][][] temporalBFS;
+    public static  int[][][] distance;
     public static int[][][][] d;
+    public static  int oo = 1000*1000;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] staticCycle = fillStaticCycle(n);
-        temporalBFS = new int[n][n][2*n];
-        d = new int[n][n][n][4*n];
+        distance = new int[n][n][4*n];
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n;j++){
+                for (int k=0; k< 3*n; k++){
+                    distance[i][j][k] = oo;
+                }
+            }
+        }
+        d = new int[n][n][2][4*n];
         createTemporalCycle(staticCycle);
-        for(int t=0; t<2*n; t++){
-            for(int s=0; s<n; s++){
-                temporalBFS(s,t,n);
-                System.out.println("okkkkkk");
+
+        for(int time=0;time<4*n;time++){
+            for(int node =0; node<n; node++){
+                temporalBFS(node, time, n);
             }
         }
 
-//        explore(n);
-//        for(int i=0; i<n; i++){
-//            for(int j=0; j<n; j++){
-//                for(int k=0; k<n; k++){
-//                    for(int t=0; t<2*n; t++){
-//                        System.out.println(d[i][j][k][t]);
-//                    }
-//                }
-//            }
-//        }
+        for(int l=0; l<4*n; l++){
+            for(int m=0; m<n; m++){
+                for(int o=0; o<n; o++){
+                    distance[m][o][l] -= l;
+                }
+            }
+        }
+        explore(n);
     }
 
     public static void createTemporalCycle(int[][] staticCycle){
         int n = staticCycle.length;
         Random rand = new Random();
-        for(int i = 0; i< 3*n; i++){
+        for(int i = 0; i< 4*n; i++){
             int rnd = rand.nextInt(n);
             int[][] cycle = copyArray(staticCycle,n);
             cycle[rnd][(rnd+1)%n] = 0;
@@ -68,54 +77,107 @@ public class Explore {
         return staticCycle;
     }
     public static void temporalBFS(int u, int t, int n){
-        ArrayList<Integer> q = new ArrayList<>();
-        boolean[] mark = new boolean[n];
-        int[] firstTime = new int[n];
-        q.add(u);
-        mark[u] = true;
-        firstTime[u] = t;
+        ArrayList<NodeTime> q = new ArrayList<>();
+        q.add(new NodeTime(u,t - 1));
         int node;
+        int time;
+
+        distance[u][u][t] = t;
         while(!q.isEmpty()){
-            node =  q.get(0);
+            node =  q.get(0).getNode();
+            time = q.get(0).getTime();
             q.remove(0);
-            System.out.println(node);
             for(int i=0; i<n; i++){
-                if((cycles.get(t)[node][i] == 1) && !mark[i]){
-                    q.add(i);
-                    mark[i] = true;
-                    firstTime[i] = findTheFirstTime(u, i, n);
+                int firstTime = findTheFirstTime(node, i, n, time)+1;
+                if(distance[u][i][t] > firstTime){
+                    distance[u][i][t] = firstTime;
+
+                    q.add(new NodeTime(i, firstTime));
                 }
+
             }
-            t++;
         }
     }
 
-    public static int findTheFirstTime(int u, int i, int n){
-        int[][] d = fillStaticCycle(n);
-        int t=0;
-        for(int p=0; p<n; p++){
+    public static int findTheFirstTime(int u, int i, int n, int t){
+        int time = oo;
+        for(int p=t+1; p<4*n; p++){
             if(cycles.get(p)[u][i] == 1){
-                t=p;
+                time=p;
                 break;
             }
         }
-        return t;
+        return time;
     }
 
     public static void explore(int n){
+        for(int x=0; x<2*n; x++){
+            for(int y=0; y<n-1; y++){
+                d[y][y+1][1][x] = distance[y][y+1][x];
+            }
+            d[n-1][0][1][x] = distance[n-1][0][x];
+        }
+
         for(int t=0; t< 2*n; t++){
             for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){
-                    for(int k=0; k<n; k++){
-                        if(i==0 || j==0){
-                            d[i][j][k][t] = 0;
+                for(int j=0; j<n-1; j++){
+                    for(int k=0; k<2; k++){
+                        int left, right;
+                        if(k==0){
+                            right = distance[i][increment(j,n)][t] +
+                                    d[i][increment(j,n)][1][(t+distance[j][increment(j,n)][t])];
+                            left = distance[i][decrement(i,n)][t] +
+                                    d[decrement(i,n)][j][0][(t+distance[i][decrement(i,n)][t])];
                         }
-                        int left = temporalBFS[j][j+1][t] + d[i][j+1][j+1][(t+temporalBFS[j][j+1][t])];
-                        int right = temporalBFS[j][i-1][t] + d[i-1][j][i-1][(t+temporalBFS[j][j+1][t])];
-                        d[i][j][k][t] = Math.min(left, right);
+                        else{
+                            right = distance[j][increment(j,n)][t] +
+                                    d[i][increment(j,n)][1][(t+distance[j][increment(j,n)][t])];
+                            left = distance[j][decrement(i,n)][t] +
+                                    d[decrement(i,n)][j][0][(t+distance[j][increment(j,n)][t])];
+                        }
+                        d[i][j][k][t] = (left < right) ? left : right;
                     }
                 }
             }
         }
     }
+
+    public static int increment(int number, int n){
+        if(number == (n-1))
+            return 0;
+        else
+            return (number+1);
+    }
+    public static int decrement(int number, int n){
+        if(number == 0)
+            return (n-1);
+        else
+            return (number-1);
+    }
+    public static void cycleWriter(int n){
+        try {
+            File myObj = new File("temporal-cycle.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter myWriter = new FileWriter("filename.txt");
+            for(int s=0; s<cycles.size();s++){
+                myWriter.write("Snapshot: " + s );
+                for(int i=0; i<n; i++){
+                    for(int j=0; j<n; j++){
+                        myWriter.write(cycles.get(s)[i][j] + " ");
+                    }
+                    myWriter.write("\n");
+                }
+            }
+            myWriter.close();
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/NodeTime.java
@@ -0,0 +1,26 @@
+public class NodeTime {
+    private int node;
+    private int time;
+
+
+    public NodeTime(int node, int time) {
+        this.node = node;
+        this.time = time;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getNode() {
+        return node;
+    }
+
+    public void setNode(int node) {
+        this.node = node;
+    }
+}
